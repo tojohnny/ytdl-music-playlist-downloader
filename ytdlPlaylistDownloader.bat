3238c4fa86750bfcd0d728e89131fd920913e17e
Johnny To <johnny.to@outlook.com>
2020-03-30 11:05:09 -0400

    Formatting, added more playlists, and reordered listing.

--- ytdlPlaylistDownloader.bat
+++ ytdlPlaylistDownloader.bat
@@ -3,29 +3,25 @@
 ::*********************************************************
 :: Configurations are based on local config file found in %APPDATA%
 :: Bat files need to be in same folder as youtube-dl.
-
 @echo off
 Title Personal YouTube-dl playlist downloader // Audio Only
 :: Set location of custom youtube-dl config file. Default locations can be found in documentation.
 set config="D:\OneDrive\Music\youtube-dl\config.txt"
-
 ECHO #######################
 ECHO # Available Playlists #
 ECHO #######################
-
 ECHO.
-
-ECHO 1. Official Artists (Japanese)
-ECHO 2. Non-Official Artists (Japanese)
-ECHO 3. Official Artists (Korean)
-ECHO 4. Non-Official Artists (Korean)
-ECHO 5. Official Artists (English)
-ECHO 6. Non-Official Artists (English)
-
+ECHO 1. Official Artists (English)
+ECHO 2. Official Artists (Chinese)
+ECHO 3. Official Artists (Japanese)
+ECHO 4. Official Artists (Korean)
+ECHO 5. Non-Official Artists (English)
+ECHO 6. Non-Official Artists (Chinese)
+ECHO 7. Non-Official Artists (Japanese)
+ECHO 8. Non-Official Artists (Korean)
+ECHO 9. Soundtracks / OST / Mixes / Off-vocals & Instrumentals
 ECHO.
-
 ECHO Which playlist would you like to download?
-
 set choice=
 set /p choice=Playlist: 
 if '%choice%'=='1' goto playlistOne
@@ -34,12 +30,54 @@ if '%choice%'=='3' goto playlistThree
 if '%choice%'=='4' goto playlistFour
 if '%choice%'=='5' goto playlistFive
 if '%choice%'=='6' goto playlistSix
+if '%choice%'=='7' goto playlistSeven
+if '%choice%'=='8' goto playlistEight
+if '%choice%'=='9' goto playlistNine
 ECHO.
-
 :playlistOne
+ECHO.
+ECHO Downloading Official Artists (English)...
+youtube-dl --config-location %config% https://www.youtube.com/playlist?list=PLVODPKIVct1NYnNLC_vC10DypFS5Bnw1Q
+ECHO.
+:playlistTwo
+ECHO.
+ECHO Downloading Official Artists (Chinese)...
+youtube-dl --config-location %config% https://www.youtube.com/playlist?list=PLVODPKIVct1P8b9IUlzbRNI8HIikkuUe4
+ECHO.
+:playlistThree
 ECHO.
 ECHO Downloading Official Artists (Japanese)...
 youtube-dl --config-location %config% https://www.youtube.com/playlist?list=PLVODPKIVct1MyQNMoCrczAl2lREwRAmNC
+ECHO.
+:playlistFour
+ECHO.
+ECHO Downloading Official Artists (Korean)...
+youtube-dl --config-location %config% https://www.youtube.com/playlist?list=PLVODPKIVct1NfSNzrzxT05dfGuMLIiim8
+ECHO.
+:playlistFive
+ECHO.
+ECHO Downloading Non-Official Artists (English)...
+youtube-dl --config-location %config% https://www.youtube.com/playlist?list=PLVODPKIVct1NJEoD9_AZmncR_6aT9xf1L
+ECHO.
+:playlistSix
+ECHO.
+ECHO Downloading Non-Official Artists (Chinese)...
+youtube-dl --config-location %config% https://www.youtube.com/playlist?list=PLVODPKIVct1M-HA8I-vQ5e1CMiu-bD-2z
+ECHO.
+:playlistSeven
+ECHO.
+ECHO Downloading Non-Official Artists (Japanese)...
+youtube-dl --config-location %config% https://www.youtube.com/playlist?list=PLVODPKIVct1O63g__-6I5WCk0gXcxAZ0O
+ECHO.
+:playlistEight
+ECHO.
+ECHO Downloading Non-Official Artists (Korean)...
+youtube-dl --config-location %config% https://www.youtube.com/playlist?list=PLVODPKIVct1M9dme_crapjyGGAa7RkjsQ
+ECHO.
+:playlistNine
+ECHO.
+ECHO Downloading Soundtracks / OST / Mixes / Off-vocals & Instrumentals...
+youtube-dl --config-location %config% https://www.youtube.com/playlist?list=PLVODPKIVct1OKVfN86nyQZq6QdKeTOoRa
 ECHO.
 :end
 pause
